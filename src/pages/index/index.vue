a47377966996086c8b7f5f16c641808db7f74676
xinconan <xinconan2@sina.com>
2018-06-09 20:25:48 +0800

    feat: 列表页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container">
-
-    
+    <Card v-for="item in [1,2,3]" :key="item"></Card>
   </div>
 </template>
 
 <script>
-
+import Card from "@/components/card.vue"
 export default {
   data () {
     return {
@@ -15,29 +14,10 @@ export default {
   },
 
   components: {
-    // card
+    Card
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
-    },
-    getUserInfo () {
-      // 调用登录接口
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
-            }
-          })
-        }
-      })
-    },
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
-    }
   },
 
   created () {
@@ -47,39 +27,5 @@ export default {
 </script>
 
 <style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-
-.counter {
-  display: inline-block;
-  margin: 10px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
-}
 </style>
--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,55 +1,159 @@
 <template>
   <div class="user-container">
-    <div class="avatar-wrapper">
+    <div class="avatar-wrapper" v-if="logged">
       <div class="avatar">
-        <open-data type="userAvatarUrl"></open-data>
+        <img :src="userInfo.avatarUrl" alt="">
       </div>
-      <open-data type="userNickName"></open-data>
+      <p>{{userInfo.nickName}}</p>
     </div>
+
+    <!-- <button v-if="!logged" open-type="getUserInfo" lang="zh_CN" @getuserinfo="onGotUserInfo">登录</button> -->
+    <button v-if="!logged"  @click="login">登录</button>
 
     <YearProgress></YearProgress>
 
-    <button type="primary" @click="scan">扫码添加图书</button>
+    <button v-if="logged" type="primary" @click="scan">扫码添加图书</button>
   </div>
 </template>
 
 <script>
 import YearProgress from '@/components/yearProgress.vue'
+import qcloud from 'wafer2-client-sdk'
+import utils from '@/utils/index'
+import config from '../../config';
 export default {
   components: {
     YearProgress
   },
 
   data () {
-    // const day = new Date()
-    // const year = day.getFullYear()
-    // const startDay = new Date(year, 0, 1)
-    // let rest = day.getTime() - startDay.getTime()
-    // rest = Math.ceil(rest / (3600 * 24000))
-    // let fullDay = this.isLeapYear(year) ? 366 : 365
-    // let passCent = Math.toFixed(rest / fullDay * 100)
-    // return {
-    //   year,
-    //   passDay: rest,
-    //   passCent
-    // }
+    return {
+      userInfo: {},
+      logged: false
+    }
   },
   methods: {
+    // 扫码添加图书
     scan () {
       wx.scanCode({
         scanType: ['qrCode'],
         success: (res) => {
-          console.log(res)
-          wx.showModal({
-            showCancel: false,
-            content: res.result
-          })
+          if (res.result) {
+            this.addBook(res.result)
+          }
         }
       })
+    },
+    async addBook (isbn) {
+      try {
+        const res = await utils.post('/weapp/book/add', {
+          isbn,
+          openId: this.userInfo.openId
+        })
+        utils.showModal('添加成功', `${res.title}添加成功`)
+      } catch (e) {
+        console.log(e)
+        // utils.showModal('添加失败', e.data.msg || '网络错误')
+      }
+    },
+    // login () {}
+    onGotUserInfo (e) {
+      const mp = e.mp.detail
+      console.log(mp)
+      const userInfo = mp.userInfo
+      // 查看是否授权
+      wx.getSetting({
+        success: (res) => {
+          if (res.authSetting['scope.userInfo']) {
+            // 检查登录是否过期
+            wx.checkSession({
+              success: () => {
+                // 登录态未过期
+                this.userInfo = userInfo
+                this.logged = true
+                // that.setData({
+                //   userInfo: userInfo,
+                //   logged: true
+                // })
+              },
+
+              fail: () => {
+                qcloud.clearSession()
+                // 登录态已过期，需重新登录
+                const options = {
+                  encryptedData: mp.encryptedData,
+                  iv: mp.iv,
+                  userInfo: userInfo
+                }
+                this.doLogin(options)
+              }
+            })
+          } else {
+            utils.showModal('用户未授权', JSON.stringify(mp.errMsg))
+          }
+        }
+      })
+    },
+    doLogin (options) {
+      wx.login({
+        success: (loginResult) => {
+          const loginParams = {
+            code: loginResult.code,
+            encryptedData: options.encryptedData,
+            iv: options.iv
+          }
+          qcloud.requestLogin({
+            loginParams,
+            success () {
+              utils.showSuccess('登录成功')
+              this.userInfo = options.userInfo
+              this.logged = true
+            }
+          })
+        },
+        fail: (err) => {
+          utils.showModal('登录失败', JSON.stringify(err))
+        }
+      })
+    },
+    // 老的获取用户信息接口
+    login () {
+      let user = wx.getStorageSync('userInfo')
+      if (!user) {
+        qcloud.login({
+          success: (userInfo) => {
+            console.log(userInfo)
+            qcloud.request({
+              url: config.userUrl,
+              login: true,
+              success: (res) => {
+                console.log(res)
+                wx.setStorageSync('userInfo', res.data.data)
+                this.userInfo = userInfo
+                this.logged = true
+              }
+            })
+          },
+          fail: (err) => {
+            utils.showModal('登录失败', JSON.stringify(err))
+          }
+        })
+      }
     }
   },
 
   created () {
+    // utils.get('/user').then((res) => {
+    //   console.log(res)
+    // })
+  },
+  onShow () {
+    console.log('hh')
+    let userInfo = wx.getStorageSync('userInfo')
+    if (userInfo) {
+      this.userInfo = userInfo
+      this.logged = true
+    }
   }
 }
 </script>
@@ -72,7 +176,7 @@ export default {
     font-size: 14rpx;
   }
 }
-.avatar{
+.avatar img{
   width: 240rpx;
   height: 240rpx;
 }
