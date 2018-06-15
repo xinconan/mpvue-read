74f0eb21f22b6feafbca8ee5b5f0400756214b5f
xinconan <xinconan2@sina.com>
2018-06-15 15:25:21 +0800

    feat: 使用chunk分割

--- src/components/TopSwiper.vue
+++ src/components/TopSwiper.vue
@@ -26,8 +26,12 @@ export default {
   props: ['tops'],
   computed: {
     imgUrls () {
-      const res = this.tops
-      return [res.slice(0,3), res.slice(3,6), res.slice(6)]
+      let res = this.tops || []
+      // 最多只显示9条数据
+      if (res.length > 9) {
+        res = res.slice(0,9)
+      }
+      return this.chunk(res, 3)
     }
   },
   methods: {
@@ -35,6 +39,47 @@ export default {
       wx.navigateTo({
         url: `/pages/detail/main?id=${img.id}`
       })
+    },
+    // lodash chunk
+    chunk(array, size) {
+      size = Math.max(size, 0)
+      const length = array == null ? 0 : array.length
+      if (!length || size < 1) {
+        return []
+      }
+      let index = 0
+      let resIndex = 0
+      const result = new Array(Math.ceil(length / size))
+
+      while (index < length) {
+        result[resIndex++] = this.slice(array, index, (index += size))
+      }
+      return result
+    },
+    slice(array, start, end) {
+      let length = array == null ? 0 : array.length
+      if (!length) {
+        return []
+      }
+      start = start == null ? 0 : start
+      end = end === undefined ? length : end
+
+      if (start < 0) {
+        start = -start > length ? 0 : (length + start)
+      }
+      end = end > length ? length : end
+      if (end < 0) {
+        end += length
+      }
+      length = start > end ? 0 : ((end - start) >>> 0)
+      start >>>= 0
+
+      let index = -1
+      const result = new Array(length)
+      while (++index < length) {
+        result[index] = array[index + start]
+      }
+      return result
     }
   }
 }
